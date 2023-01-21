40c232f6fabf00786034f9e706b3e293d3f4e975
Zem Hysong <zlhysong@gmail.com>
2023-01-21 00:02:40 -0600

    Take a screenshot and save to the same folder as this file with Print Screen.

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+# This is a script to help export screenshots into Blender from DOSBox, PCem, etc.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Required Libraries: global-hotkeys, pypiwin32, pyautogui, pillow
+
+from global_hotkeys import *
+import pyautogui
+import time
+
+is_alive = True  # Flag to indicate to the program whether it should continue running.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def take_screenshot():
+    pyautogui.screenshot('my_screenshot.png')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def up_action():
+    print('up_action pressed')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def down_action():
+    print('down_action pressed')
+
+
+def left_action():
+    print('left_action pressed')
+
+
+def right_action():
+    print('right_action pressed')
+
+
+def exit_application():
+    global is_alive
+    stop_checking_hotkeys()
+    is_alive = False
+
+
+bindings = [  # [<key list>, <keydown handler callback>, <keyup handler callback>]
+    [["print_screen"], None, take_screenshot],
+    [["up"], None, up_action],
+    [["down"], None, down_action],
+    [["left"], None, left_action],
+    [["right"], None, right_action],
+    [["control", "print_screen", "shift"], None, exit_application],
+]
+
+
+register_hotkeys(bindings)  # Register all of our keybindings
+start_checking_hotkeys()    # Start listening for keypresses
+
+
+while is_alive:  # Run until closed
+    time.sleep(0.1)
